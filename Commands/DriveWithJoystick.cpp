dabe44c2690bb294d31d4f6142784c1dd8990d8a
David <jonie4@gmail.com>
2013-01-26 15:09:59 -0800

    Added commands, and has some preliminary drivetrain code.
    -David

--- Commands/DriveWithJoystick.cpp
+++ Commands/DriveWithJoystick.cpp
@@ -26,7 +26,7 @@ void DriveWithJoystick::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void DriveWithJoystick::Execute() {
-	drivetrain->
+	drivetrain->tankDrive();
 }
 
 // Make this return true when this Command no longer needs to run execute()
