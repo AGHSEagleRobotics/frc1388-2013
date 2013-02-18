1cdc0779ceb71acbfd5605097ebae2fd8bcac5a7
Joel Braun <joelsbraun@gmail.com>
2013-02-17 16:18:29 -0800

    Fixed merge issue, am now on updated code locally.
    Joel

--- Subsystems/Climber.cpp
+++ Subsystems/Climber.cpp
@@ -46,10 +46,7 @@ void Climber::TapesInOut(float opstickx, bool opTapesIn, bool opTapesOut){
 */		
 	// && tapeLimitLeft == false && tapeLimitRight == false FOR LATE
 	
-<<<<<<< HEAD
 
-=======
->>>>>>> b1022992595f18c9b629a70617d89ce35115d77e
 if (opTapesIn == true )
 	{
 		
